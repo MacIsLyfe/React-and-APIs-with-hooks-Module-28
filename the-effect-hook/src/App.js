a09633e5f9ed7cf4031d97e3346f883a82179e04
Mac Cooke <101891594+MacIsLyfe@users.noreply.github.com>
2022-10-26 01:45:22 -0400

    Add files via upload

---
+++ the-effect-hook/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+
+// Load data from https://jsonplaceholder.typicode.com/todos?userId=2
+function App() {
+  const [toDos, setToDos] = useState([]);
+
+ useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/todos?userId=2")
+    .then((response) => response.json())
+    .then(setToDos);
+}, []);
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <ul className="todo-list">
+        {toDos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "",
+            }}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
